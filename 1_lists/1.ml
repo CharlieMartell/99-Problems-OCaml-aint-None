158d03b3a1cabf62048aabfcbb429c4e4f97f876
CharlieMartell <charliecmartell@gmail.com>
2015-08-10 22:58:55 -0700

    first problem

---
+++ 1_lists/1.ml
@@ -0,0 +1,8 @@
+(*Write a function last : 'a list -> 'a option 
+ * that returns the last element of a list.*) 
+
+let rec last = function
+  | [] -> None
+  | [x] -> Some x
+  | _ :: y -> last y;;
+
