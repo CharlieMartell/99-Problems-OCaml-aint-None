4c9917277c56ed0242bc42fbefc3a50a7fb88995
CharlieMartell <charliecmartell@gmail.com>
2015-08-13 14:28:25 -0700

    9

---
+++ 1_lists/9.ml
@@ -0,0 +1,35 @@
+(* Pack consecutive duplicates of list elements into sublists. *)
+
+(* 
+Examples from the book
+# pack ["a";"a";"a";"a";"b";"c";"c";"a";"a";"d";"d";"e";"e";"e";"e"];;
+- : string list list =
+  [["a"; "a"; "a"; "a"]; ["b"]; ["c"; "c"]; ["a"; "a"]; ["d"; "d"];
+   ["e"; "e"; "e"; "e"]]
+*)
+
+let pack lst =
+  let rev l =
+    let rec rrev rest = function
+      | [] -> rest
+     | x :: xs -> rrev (x::rest) xs
+    in rrev [] l
+  in 
+  let rec rpack pre suf cur_val lst =
+     match lst with
+       | [] -> rev (suf :: pre)
+       | x :: xs when x = cur_val -> rpack pre (x::suf) cur_val xs
+       | x :: xs -> rpack (suf::pre) [x] x xs
+  in match lst with
+    | [] -> []
+    | x :: xs -> rpack [] [x] x xs
+;;
+
+(* Tests all cases *)
+assert (pack ["a";"a";"a";"a";"b";"c";"c";"a";"a";"d";"d";"e";"e";"e";"e"] =
+            [["a"; "a"; "a"; "a"]; ["b"]; ["c"; "c"]; ["a"; "a"]; ["d"; "d"];
+            ["e"; "e"; "e"; "e"]]);;
+assert (pack ["a"] = [["a"]]);;
+assert (pack [] = [[]]);;
+
+
