7c0567cc23a4b24bd3256e58e00b28e5beda8a0a
CharlieMartell <charliecmartell@gmail.com>
2015-08-15 13:41:58 -0700

    10 done

---
+++ 1_lists/10.ml
@@ -0,0 +1,32 @@
+(* Run-length encoding of a list. *)
+
+(* 
+Examples from the book
+# encode ["a";"a";"a";"a";"b";"c";"c";"a";"a";"d";"e";"e";"e";"e"];;
+- : (int * string) list =
+  [(4, "a"); (1, "b"); (2, "c"); (2, "a"); (1, "d"); (4, "e")]
+*)
+
+let encode lst =
+  let rev l =
+    let rec rrev rest = function
+      | [] -> rest
+      | x :: xs -> rrev (x::rest) xs
+    in rrev [] l
+  in 
+    let rec rencode sum lst =
+      match sum, lst with
+        | _, [] -> sum
+        | [], x::xs -> rencode [(1, x)] xs
+        | (num, y)::ys, x::xs -> if x = y
+                                 then rencode (((num + 1), y)::ys) xs
+                                 else rencode ((1, x)::sum) xs
+      in rev (rencode [] lst)
+;;
+
+(* Tests all cases *)
+assert (encode ["a";"a";"a";"a";"b";"c";"c";"a";"a";"d";"e";"e";"e";"e"] =
+  [(4, "a"); (1, "b"); (2, "c"); (2, "a"); (1, "d"); (4, "e")]);;
+assert (encode ["a"; "a"; "b"; "c"] = [(2, "a"); (1, "b"); (1,"c")]);;
+assert (encode ["a"] = [(1,"a")]);;
+assert (encode [] = []);;
--- 1_lists/9.ml
+++ 1_lists/9.ml
@@ -15,18 +15,18 @@ let pack lst =
       | x :: xs -> rrev (x::rest) xs
     in rrev [] l
   in 
-  let rec rpack pre suf cur_val lst =
-     match lst with
-       | [] -> rev (suf :: pre)
-       | x :: xs when x = cur_val -> rpack pre (x::suf) cur_val xs
-       | x :: xs -> rpack (suf::pre) [x] x xs
-  in match lst with
-    | [] -> []
-    | x :: xs -> rpack [] [x] x xs
+    let rec rpack pre suf cur_val lst =
+       match lst with
+         | [] -> rev (suf :: pre)
+         | x :: xs when x = cur_val -> rpack pre (x::suf) cur_val xs
+         | x :: xs -> rpack (suf::pre) [x] x xs
+    in match lst with
+      | [] -> []
+      | x :: xs -> rpack [] [x] x xs
 ;;
 
 (* Tests all cases *)
- assert (pack ["a";"a";"a";"a";"b";"c";"c";"a";"a";"d";"d";"e";"e";"e";"e"] =
-            [["a"; "a"; "a"; "a"]; ["b"]; ["c"; "c"]; ["a"; "a"]; ["d"; "d"];
-            ["e"; "e"; "e"; "e"]]);;
+assert (pack ["a";"a";"a";"a";"b";"c";"c";"a";"a";"d";"d";"e";"e";"e";"e"] =
+             [["a"; "a"; "a"; "a"]; ["b"]; ["c"; "c"]; ["a"; "a"]; ["d"; "d"];
+             ["e"; "e"; "e"; "e"]]);;
 assert (pack ["a"] = [["a"]]);;
