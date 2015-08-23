5e8af69f15de1e666dadced9819c0197b018d25c
CharlieMartell <charliecmartell@gmail.com>
2015-08-23 18:02:52 -0500

    18 beginning

---
+++ 1_lists/18.ml
@@ -0,0 +1,12 @@
+(* Extract a slice from a list. *)
+
+(* 
+Examples from the book
+# split ["a";"b";"c";"d";"e";"f";"g";"h";"i";"j"] 3;;
+- : string list * string list =
+  (["a"; "b"; "c"], ["d"; "e"; "f"; "g"; "h"; "i"; "j"])
+# split ["a";"b";"c";"d"] 5;;
+- : string list * string list = (["a"; "b"; "c"; "d"], [])
+*)
+
+
